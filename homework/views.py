3f568ecd4f2a0c85e8ba8a138430f41431df50d2
ZhainaErkin <zhaynagul.erkinbekova.ch@gmail.com>
2023-04-15 12:45:05 +0600

    init

---
+++ homework/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+
+def homeworkk (request):
+    return render(request,"homeworkk.html")
+
+def homepage(request):
+    return HttpResponse('Добро пожаловать в приложение ToDo - Admin')
