8e3da4d3f89f5a81b40ba7c61a0ed990f3d31aca
ZhainaErkin <zhaynagul.erkinbekova.ch@gmail.com>
2023-04-16 13:38:55 +0600

    set ststic and media urls and roots

--- todo/urls.py
+++ todo/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from homework.views import homepage,homeworkk,homepage,check
-from django.shortcuts import settings
+from django.conf import settings
 from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +28,4 @@ urlpatterns = [
     path ("homeworkk",homeworkk,name="homeworkk"),
     path("", homepage, name="home"),
     path("check",check)
-]+static(settings.STATIC_URL,document_roat=settings.STATIC_BOOT)
-+static(settings.STATIC_URL,document_roat=settings.STATIC_BOOT)
+]   +static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
