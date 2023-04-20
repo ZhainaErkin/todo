d83d0d5d06844dd02630759cd8779c73e3d7ecaa
ZhainaErkin <zhaynagul.erkinbekova.ch@gmail.com>
2023-04-20 16:36:50 +0600

    added todo model and admin

--- homework/admin.py
+++ homework/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from .models import ToDo
 
-# Register your models here.
+admin.site.register(ToDo)
+
+
+
+
+from .models import ToMeet
+
+class ToMeetAdmin(admin.ModelAdmin):
+    list_display = ('person', 'phone_number', 'date_of_meeting', 'is_closed', 'is_favorite')
+    list_filter = ('is_closed', 'is_favorite')
+    search_fields = ('person', 'phone_number')
+
+admin.site.register(ToMeet, ToMeetAdmin)
+
+
+
+
+from django import models
+
+class Person(models,):
+    name=models.CharField(max_length=20)
+    age=models.IntegerField()
+    
+
+admin.site.register( Person,)
+
+
+# Задание № 3
+
+# Напишите тип данных каждого из перечисленных типов моделей django.
+
+# models.BooleanField()
+# models.DateTimeField()
+# models.FloatField()
+# models.CharField()
+# models.EmailField()
+# models.TextField()
+
+
+# Каждый из перечисленных типов полей в Django соответствует 
+# определенному типу данных в базе данных:
+
+# models.BooleanField() соответствует типу данных BOOLEAN в базе данных.
+# models.DateTimeField() соответствует типу данных DATETIME в базе данных.
+# models.FloatField() соответствует типу данных FLOAT в базе данных.
+# models.CharField() соответствует типу данных VARCHAR в базе данных. 
+# Этот тип поля позволяет хранить строковые данные переменной длины.
+# models.EmailField() также соответствует типу данных VARCHAR в базе данных.
+#  Он используется для хранения email-адресов и включает проверку на соответствие формату email-адреса.
+# models.TextField() также соответствует типу данных TEXT в базе данных.
+# Он используется для хранения больших объемов текстовых данных.
+
+
+
+
--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+  text = models.CharField(max_length=100)
+  date=models.DateTimeField(auto_now_add=False)
+  is_closed =models.BooleanField(default=False)
+  is_favorite=models.BooleanField(default=False)
+
+
+
+  
+
+class ToMeet(models.Model):
+    person = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    date_of_meeting = models.DateField()
+    comment = models.TextField(blank=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
